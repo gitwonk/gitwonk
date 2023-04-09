8f9d3b0a5a0cf38f81d155c09e2cae2f2ad0e8b6
Yeganathan S <hilariousengineer@protonmail.com>
2023-04-09 19:58:00 +0530

    fix: validate email using regex for inputs

--- apps/www/components/home-input.tsx
+++ apps/www/components/home-input.tsx
@@ -12,7 +12,8 @@ const HomeInput = () => {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!email.includes("@")) {
+    const regex = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+    if (!regex.test(email)) {
       toast.error("Please enter a valid email");
       return;
     }
