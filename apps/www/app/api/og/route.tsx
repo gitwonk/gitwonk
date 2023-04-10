cad18621f91e5fbf7cce67a4fa79a8dc47f764c5
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-04-10 12:28:42 +0000

    feat: add custom og image

--- apps/www/app/api/og/route.tsx
+++ apps/www/app/api/og/route.tsx
@@ -4,26 +4,37 @@ export const config = {
   runtime: "edge",
 };
 
+const satoshi = fetch(
+  new URL("../../../public/fonts/Satoshi-Black.ttf", import.meta.url),
+).then((res) => res.arrayBuffer());
+
 export async function GET(request) {
+  const [satoshiFont] = await Promise.all([satoshi]);
+
   return new ImageResponse(
     (
-      <div
-        style={{
-          fontSize: 128,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          textAlign: "center",
-          alignItems: "center",
-          justifyContent: "center",
-        }}>
-        Hello world!
+      <div tw="flex items-end justify-between p-16 h-full w-full">
+        <h1 tw="text-8xl font-extrabold max-w-lg">
+          Write, Collaborate, Publish.
+        </h1>
+
+        <img
+          width="180"
+          height="180"
+          tw="justify-end"
+          src="https://gitwonk.com/images/avatars/gitwonk.png"
+        />
       </div>
     ),
     {
       width: 1200,
-      height: 600,
+      height: 630,
+      fonts: [
+        {
+          name: "Satoshi",
+          data: satoshiFont,
+        },
+      ],
     },
   );
 }
