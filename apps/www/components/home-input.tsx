e1edb9a71e7dd3e02016228d4c4b5f220b3df020
Nayan Thara M <wittyhuminid@protonmail.com>
2023-08-20 00:27:48 +0530

    fix: support for any email domains (#14)

--- apps/www/components/home-input.tsx
+++ apps/www/components/home-input.tsx
@@ -12,7 +12,7 @@ const HomeInput = () => {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const regex = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+    const regex = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i);
     if (!regex.test(email)) {
       toast.error("Please enter a valid email");
       return;
